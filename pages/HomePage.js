369c0bfe5da63fa2dc13cb6d689943c3fe3f62d1
yetkin <yetkinsonmez45@gmail.com>
2023-04-05 03:45:15 +0300

    homepage buildings added from backend

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,33 +1,73 @@
-import { View, StatusBar, StyleSheet, Text, Image, TextInput, ScrollView, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
-import BuildingList from "../Backend/buildingServicesComponent/buildingList";
+import {
+  View,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { loadBuildings } from "../Backend/buildingServices";
 
 function HomePage({ navigation }) {
   const goNewDevice = () => {
-    console.log('Configure a new device button pressed');
-    navigation.navigate('WifiSearch');
-  }
+    console.log("Configure a new device button pressed");
+    navigation.navigate("WifiSearch");
+  };
 
   const goProfile = () => {
-    console.log('Profile icon pressed');
-    navigation.navigate('Profile');
-  }
+    console.log("Profile icon pressed");
+    navigation.navigate("Profile");
+  };
 
   const goLogin = () => {
-    console.log('Logout icon pressed');
-    navigation.navigate('Login');
-  }
+    console.log("Logout icon pressed");
+    navigation.navigate("Login");
+  };
 
   const [searchText, setSearchText] = useState("");
-  const [filteredBoxes, setFilteredBoxes] = useState(["FENS0001", "FASS0001", "FASS0002", "FASS0003", "FASS0004",]);
+  const [filteredBuildings, setFilteredBuildings] = useState([]);
+  const [buildings, setBuildings] = useState([]);
+
+  useEffect(() => {
+    const fetchBuildings = async () => {
+      const data = await loadBuildings();
+      setBuildings(data);
+      setFilteredBuildings(data);
+    };
+    fetchBuildings();
+  }, []);
 
   const handleSearch = (text) => {
     setSearchText(text);
-    const filtered = ["FENS0001", "FASS0001", "FASS0002", "FASS0003", "FASS0004"].filter((box) =>
-      box.toLowerCase().includes(text.toLowerCase())
+    const filtered = buildings.filter((building) =>
+      building.name.toLowerCase().includes(text.toLowerCase())
     );
-    setFilteredBoxes(filtered);
+    setFilteredBuildings(filtered);
   };
+
+  const Building = ({ building }) => {
+    const onPress = () => {
+      console.log(`Building ${building.name} pressed`);
+      // Navigate to building details screen
+      // navigation.navigate('BuildingDetails', { buildingId: building.id });
+    };
+    return (
+      <TouchableOpacity
+        style={homePageStyles.buildingRow}
+        onPress={onPress}
+        key={building.id}
+      >
+        <Text style={homePageStyles.buildingName}>{building.name}</Text>
+        <Text>{building.address}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderBuilding = ({ item }) => <Building building={item} />;
 
   return (
     <>
@@ -36,165 +76,155 @@ function HomePage({ navigation }) {
         <View style={homePageStyles.bodyContainer}>
           <View style={homePageStyles.appBar}>
             <View style={homePageStyles.iconPosition}>
-            <TouchableOpacity onPress={goProfile}>
-              <Image
-                style={homePageStyles.icon}
-                source={require("../assets/icons/profile-user.png")}
-              />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={goLogin}>
-              <Image
-                style={homePageStyles.icon}
-                source={require("../assets/icons/logout.png")}
-              />
-              </TouchableOpacity>
-            </View>
             <View>
               <Text style={homePageStyles.appBarText}>MY DEVICES</Text>
             </View>
-            <View style={{ width: 25 }}/>
+            <View style={{ width: 100 }}/>
+              <TouchableOpacity onPress={goProfile}>
+                <Image
+                  style={homePageStyles.icon}
+                  source={require("../assets/icons/profile-user.png")}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={goLogin}>
+                <Image
+                  style={homePageStyles.icon}
+                  source={require("../assets/icons/logout.png")}
+                />
+              </TouchableOpacity>
+            </View>
           </View>
           <View style={homePageStyles.configureButton}>
-          <TouchableOpacity style={homePageStyles.button} onPress={goNewDevice}>
+            <TouchableOpacity style={homePageStyles.button} onPress={goNewDevice}>
               <Text style={homePageStyles.buttonText}>Configure a new device</Text>
             </TouchableOpacity>
           </View>
           <View style={homePageStyles.homePageBody}>
-          <View style={homePageStyles.searchBoxContainer}>
+            <View style={homePageStyles.searchBoxContainer}>
               <TextInput
                 style={homePageStyles.searchBox}
                 placeholder="Search"
                 value={searchText}
                 onChangeText={handleSearch}
-              />
-            </View>
-
-            {/* <View>
-              <Text>--------------------</Text>
-              <BuildingList />
-              <Text>--------------------</Text>
-            </View> */}
-
-
-            <View style={homePageStyles.staticBoxContainer}>
-              {filteredBoxes.includes("FENS0001") && (
-                <TouchableOpacity style={homePageStyles.staticBox}>
-                  <Text style={homePageStyles.staticBoxText}>FENS0001</Text>
-                </TouchableOpacity>
-              )}
-              {filteredBoxes.includes("FASS0001") && (
-                <TouchableOpacity style={homePageStyles.staticBox}>
-                  <Text style={homePageStyles.staticBoxText}>FASS0001</Text>
-                </TouchableOpacity>
-              )}
-              {filteredBoxes.includes("FASS0002") && (
-                <TouchableOpacity style={homePageStyles.staticBox}>
-                  <Text style={homePageStyles.staticBoxText}>FASS0002</Text>
-                </TouchableOpacity>
-              )}
-              {filteredBoxes.includes("FASS0003") && (
-                <TouchableOpacity style={homePageStyles.staticBox}>
-                  <Text style={homePageStyles.staticBoxText}>FASS0003</Text>
-                </TouchableOpacity>
-              )}
-              {filteredBoxes.includes("FASS0004") && (
-                <TouchableOpacity style={homePageStyles.staticBox}>
-                  <Text style={homePageStyles.staticBoxText}>FASS0004</Text>
-                </TouchableOpacity>
-              )}
+                />
+                <Image
+                  style={homePageStyles.searchIcon}
+                  source={require("../assets/icons/search.png")}
+                />
+              </View>
+              <View style={homePageStyles.buildingsContainer}>
+                {filteredBuildings.length > 0 ? (
+                  <FlatList
+                    data={filteredBuildings}
+                    renderItem={renderBuilding}
+                    keyExtractor={(building) => building.id}
+                  />
+                ) : (
+                  <Text>No buildings found</Text>
+                )}
+              </View>
             </View>
           </View>
-        </View>
-      </ScrollView>
-    </>
-  );
-}
-
-export default HomePage;
-
-const homePageStyles = StyleSheet.create({
-  bodyContainer: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "stretch",
-  },
-  appBar: {
-    flex: 1,
-    backgroundColor: "white",
-    flexDirection: "row-reverse",
-    justifyContent: "space-between",
-    alignItems: 'flex-end',
-    paddingBottom: 8,
-  },  
-  appBarText: {
-    fontSize: 36,
-    lineHeight: 45,
-    fontWeight: "bold",
-    letterSpacing: 0.3,
-    color: "#263159",
-    marginLeft: -100,
-  },  
-  button: {
-    backgroundColor: '#495579',
-    borderRadius: 50,
-    padding: 20,
-    marginVertical: 10,
-    width: 350,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  configureButton:{
-    alignItems: 'center',
-  },
-  icon: {
-    height: 25,
-    width: 25,
-    marginHorizontal: 10,
-    marginBottom: 5,
-  },
-  iconPosition: {
-    flexDirection: "row",
-    alignItems: "flex-end",
-  }, 
-  homePageBody: {
-    flex: 12,
-    backgroundColor: "white",
-  },
-  searchBox: {
-    height: 40,
-    borderWidth: 1,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    marginVertical: 10,
-  },
-  staticBoxContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    marginTop: 10,
-  },
-  staticBox: {
-    backgroundColor: "#495579",
-    width: "95%",
-    height: 50,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    margin:10,
-    marginVertical: 10,
-  },
-  staticBoxText: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "white",
-  },
-  searchBoxContainer: {
-    marginHorizontal: 10,
-  },
-});
+        </ScrollView>
+      </>
+      );
+    }
+    
+    const homePageStyles = StyleSheet.create({
+      bodyContainer: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "stretch",
+        justifyContent: "center",
+        backgroundColor: "#fff",
+        padding: 10,
+      },
+      appBar: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 10,
+      },
+      iconPosition: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+      },      
+      icon: {
+        width: 30,
+        height: 30,
+        marginLeft: 5,
+        marginRight: 15,
+      },
+      appBarText: {
+        fontSize: 32,
+        lineHeight: 36,
+        fontWeight: "bold",
+        letterSpacing: 0.2,
+        color: "#263159",
+      },  
+      configureButton: {
+        marginTop: 10,
+        marginBottom: 20,
+      },
+      button: {
+        backgroundColor: "#495579",
+        borderRadius: 5,
+        padding: 10,
+      },
+      buttonText: {
+        color: "#fff",
+        fontSize: 22,
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+      homePageBody: {
+        flexDirection: "column",
+        alignItems: "stretch",
+        justifyContent: "center",
+      },
+      searchBoxContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderWidth: 2,
+        borderColor: "#495579",
+        borderRadius: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginBottom: 20,
+      },
+      searchBox: {
+        flex: 1,
+        fontSize: 16,
+        height: 40,
+        color: "#000",
+        paddingLeft: 5,
+      },
+      searchIcon: {
+        width: 20,
+        height: 20,
+      },
+      buildingsContainer: {
+        flex: 1,
+        alignItems: "stretch",
+        justifyContent: "center",
+      },
+      buildingRow: {
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#495579",
+        borderRadius: 5,
+        marginBottom: 10,
+      },
+      buildingName: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 5,
+      },
+    });
+    
+    export default HomePage;
