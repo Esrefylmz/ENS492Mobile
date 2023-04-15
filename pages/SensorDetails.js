214454f2e9a7f84cc5811164567fd7f88748527d
Mehmet Faruk Kömürcüler <farukkomurculer@hotmail.com>
2023-04-16 02:41:26 +0300

    horizontal scroll on time-series graph.

--- pages/SensorDetails.js
+++ pages/SensorDetails.js
@@ -3,20 +3,16 @@ import { View, Text, StyleSheet, Dimensions , TouchableOpacity , ActivityIndicat
 import { GetDataByMacId } from "../Backend/sensorServices";
 import { LineChart } from 'react-native-chart-kit';
 import { loadMeasurementType } from '../Backend/measurementTypeServices';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
-function BuildingDetails({ route }) {
+function SensorDetails({ route }) {
   const { sensor } = route.params;
   console.log('Sensor Details: ', sensor)
   const [sensorData, setSensorData] = useState([]);
 
   const [measurementTypes, setMeasurementTypes] = useState([]);
 
-  
-
-  const handleDataPointClick = (dataPoint) => {
-    setSelectedValue(dataPoint);
-  };
   const filterDataByMeasurementTypeId = (data, measurementTypeId) => {
     return data.filter(item => item.measurementTypeId === measurementTypeId);
   };
@@ -45,20 +41,13 @@ function BuildingDetails({ route }) {
 
   console.log("SENSOR DATA", sensorData)
 
-  const [selectedValue, setSelectedValue] = useState(null);
+  
   const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp);
     const hours = date.getHours();
     const minutes = date.getMinutes();
     return `${hours}:${minutes}`;
   };
-
-  const lastFilteredData = filteredData.filter((item, index) => {
-    const currentDate = new Date();
-    const itemDate = new Date(item.timestamp);
-    return index % Math.ceil(filteredData.length / 10) === 0 || itemDate > currentDate;
-  });
-  
     return (
       <View style={styles.container}>
         <Text style={styles.title}>{sensor.softId}</Text>
@@ -68,81 +57,44 @@ function BuildingDetails({ route }) {
         <View style={styles.detailsContainer}>
           <Text style={styles.detailsLabel}>MACID : {sensor.macId}</Text>
         </View>
-        {lastFilteredData.length === 0 
-        ? (
-        <ActivityIndicator size="large" color="#000000" />) 
-        : (
-        <LineChart
-          data={{
-            labels: lastFilteredData.map((item) => formatTimestamp(item.timestamp)), // Format timestamp labels
-            datasets: [
-              {
-                data: lastFilteredData.map((item) => item.measurementValue),
-                color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`,
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {filteredData.length === 0 ? (
+        <ActivityIndicator size="large" color="#000000" />
+      ) : (
+        <View>
+          
+          <LineChart
+            data={{
+              labels: filteredData.map((item) => formatTimestamp(item.timestamp)), // Use formatTimestamp to format timestamps
+              datasets: [
+                {
+                  data: filteredData.map((item) => item.measurementValue),
+                  color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`,
+                },
+              ],
+            }}
+            width={Dimensions.get('window').width + 250} // Increase width to allow for horizontal scrolling
+            height={220}
+            chartConfig={{
+              backgroundGradientFrom: 'white',
+              backgroundGradientTo: 'white',
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              style: {
+                borderRadius: 16,
               },
-            ],
-          }}
-          width={Dimensions.get('window').width - 40}
-          height={220}
-          chartConfig={{
-            backgroundGradientFrom: '#ffffff',
-            backgroundGradientTo: '#ffffff',
-            decimalPlaces: 2,
-            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-          }}
-          onDataPointClick={({ value, getColor }) => {
-            handleDataPointClick(value);
-          }}
-        />
+            }}
+            onDataPointClick={({ value }) => {
+              }
+            }
+          />
+        </View>
       )}
-      {selectedValue && (
-        <TouchableOpacity
-          onPress={() => setSelectedValue(null)}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          }}
-        >
-          <Text style={{ color: '#ffffff', fontSize: 24 }}>
-            {selectedValue.toFixed(2)}
-          </Text>
-        </TouchableOpacity>
-      )}
+    </ScrollView>
     </View>);
 }
 
 const styles = StyleSheet.create({
-  dropdownContainer: {
-    position: 'relative',
-  },
-  dropdownMenu: {
-    backgroundColor: '#fff', // Set desired background color
-    borderWidth: 1, // Set desired border width
-    borderColor: '#ccc', // Set desired border color
-    borderRadius: 4, // Set desired border radius
-    padding: 8, // Set desired padding
-    maxHeight: 200, // Set desired max height for dropdown menu
-    overflowY: 'auto', // Add scrollbar for long dropdown menus
-  },
-  dropdownItem: {
-    padding: 8, // Set desired padding for dropdown items
-    cursor: 'pointer', // Add pointer cursor for clickable items
-  },
-  dropdownItemSelected: {
-    backgroundColor: '#f0f0f0', // Set desired background color for selected item
-  },
-  dropdownItemText: {
-    // Add additional styles for dropdown item text as needed
-  },
   container: {
     flex: 1,
     padding: 20,
@@ -171,4 +123,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default BuildingDetails;
+export default SensorDetails;
